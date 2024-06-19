79042414e5fc2477da2d7f70b2c5b892feea18a6
karina250 <69991519+karina250@users.noreply.github.com>
2024-06-19 17:25:04 +0000

    tercer commit

---
+++ comments.js
@@ -0,0 +1,22 @@
+//create web server object
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+//create server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+console.log('Server running at http://');
+
